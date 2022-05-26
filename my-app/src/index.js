0ee7064614c7afeec06e9c41ac55add2fa9b6f9f
Edoardo Longo <Edoardo.Longo.1999@gmail.com>
2022-05-26 10:58:12 +0200

    feature: Components 4

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,7 @@
+import HelloWorld from "./Hello";
 import ReactDOM from "react-dom";
-import App from "./App";
 
+const HelloWorldElement = <HelloWorld/>
 const root = document.querySelector('#root')
 
-ReactDOM.render(<App/>, root)
+ReactDOM.render(HelloWorldElement, root)
