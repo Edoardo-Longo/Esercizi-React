36f42b6fb4aea3f3b0bdb17acf119c9adb8fbd47
Edoardo Longo <Edoardo.Longo.1999@gmail.com>
2022-06-03 10:50:00 +0200

    Conditional Rendering 1

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import Welcome from "./Welcome";
+
+function App() {
+  return (
+    <div>
+      <Welcome name='Edoardo' age={22}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Welcome.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Age from "./Age";
+
+class Welcome extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Hello,{this.props.name}</h2>
+        {this.props.age >= 18 && <Age age={this.props.age} />}
+      </div>
+    );
+  }
+}
+
+export default Welcome;
