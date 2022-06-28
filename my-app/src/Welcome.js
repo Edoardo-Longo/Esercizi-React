2bf56d2a545ea5d9092642b532f1465999ca58a3
Edoardo Longo <Edoardo.Longo.1999@gmail.com>
2022-06-28 17:05:02 +0200

    Esercizio 1

---
+++ my-app/src/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Welcome extends React.Component{
+    render(){
+        return(
+            <div className='welcome'>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+}
+
+export default Welcome
