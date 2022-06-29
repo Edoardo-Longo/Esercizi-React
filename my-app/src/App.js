52e103e0e2ec7b05825cb351244328f88ddba68a
Edoardo Longo <Edoardo.Longo.1999@gmail.com>
2022-06-29 18:50:18 +0200

    Esercizio 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,11 @@
 import Welcome from './Welcome';
+import Sum from './Sum';
 
 function App() {
   return (
     <div className="App">
       <Welcome/>
+      <Sum numbers={[2, 5 ,8, 100]}/>
     </div>
   );
 }
