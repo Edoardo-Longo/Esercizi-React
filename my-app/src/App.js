c0d692885562b17627efd79dd517b1d86626d1d6
Edoardo Longo <Edoardo.Longo.1999@gmail.com>
2022-06-30 17:19:13 +0200

    Eserczio 4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,33 @@
-import { Route, Routes } from 'react-router-dom';
-import Welcome from './Welcome';
-import Counter from './Counter';
-import ShowGithubUser from './ShowGithubUser';
+import { Route, Routes, Link } from "react-router-dom";
+import Welcome from "./Welcome";
+import Counter from "./Counter";
+import ShowGithubUser from "./ShowGithubUser";
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element={<Welcome name='Edoardo'/>}></Route>
-        <Route path='/counter' element={<Counter/>}></Route>
-        <Route path='/githubuser'>
-          <Route path=':name' element={<ShowGithubUser />} />
-        </Route>
-      </Routes>
-    </div>
+    <>
+      <div className="App">
+        <Link to="/counter">
+          {" "}
+          Counter
+        </Link>
+        <Link to="/githubuser/Edoardo-Longo">
+          Link2
+        </Link>
+        <Link to="/">
+          {" "}
+          Welcome
+        </Link>
+
+        <Routes>
+          <Route path="/" element={<Welcome name="Edoardo" />}></Route>
+          <Route path="/counter" element={<Counter />}></Route>
+          <Route path="/githubuser">
+            <Route path=":name" element={<ShowGithubUser />} />
+          </Route>
+        </Routes>
+      </div>
+    </>
   );
 }
 
