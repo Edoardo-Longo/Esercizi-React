421f241b0fbf5b393e02f4e9bcdccab702f11228
Edoardo Longo <Edoardo.Longo.1999@gmail.com>
2022-06-30 17:33:16 +0200

    Eserczio 7

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,31 +2,24 @@ import { Route, Routes, Link } from "react-router-dom";
 import Welcome from "./Welcome";
 import Counter from "./Counter";
 import GithubUser from "./GithubUser";
-import ShowGithubUser from './ShowGithubUser';
+import ShowGithubUser from "./ShowGithubUser";
 
 function App() {
   return (
     <>
       <div className="App">
-        <Link to="/counter">
-          {" "}
-          Counter
-        </Link>
-        <Link to="/user">
-          Git
-        </Link>
-        <Link to="/">
-          {" "}
-          Welcome
-        </Link>
+        <Link to="/counter"> Counter</Link>
+        <Link to="/user">Git</Link>
+        <Link to="/"> Welcome</Link>
 
         <Routes>
           <Route path="/" element={<Welcome name="Edoardo" />}></Route>
           <Route path="/counter" element={<Counter />}></Route>
           <Route path="user" element={<ShowGithubUser />}>
+            <Route index element={<h3>Scrivi un nome e clicca il link!</h3>} />
             <Route path=":name" element={<GithubUser />} />
           </Route>
-          <Route path='*' element={<h1>Errore 404</h1>} />
+          <Route path="*" element={<h1>Errore 404</h1>} />
         </Routes>
       </div>
     </>
