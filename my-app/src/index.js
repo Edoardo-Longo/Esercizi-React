17accee7ddd0e867742cbbf60ed9f2b754f9e2e6
Edoardo Longo <Edoardo.Longo.1999@gmail.com>
2022-06-30 18:10:09 +0200

    Eserczio 3

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { store } from './state/Store';
+import { addTodo,removeTodo,editTodo } from './state/todoState';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,6 +12,11 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+store.dispatch(addTodo({ id: 1, title: 'Ho caldo', completed: true }));
+store.dispatch(addTodo({ id: 2, title: 'Voglio andare al mare', completed: true }));
+store.dispatch(editTodo(1, {title: 'HO DAVVERO TANTO CALDO', completed: true}));
+store.dispatch(removeTodo(1));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
