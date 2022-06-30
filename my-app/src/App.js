247b37e0d1305c34118011316aed50944a8dc41d
Edoardo Longo <Edoardo.Longo.1999@gmail.com>
2022-06-30 15:56:18 +0200

    useMemo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,28 @@
 import Counter from './Counter';
+import FilterdList from './FilteredList';
 import {useState} from 'react'
 
 function App() {
+
+  const [list, setList] = useState([{
+    id: 1,
+    name: 'Marco',
+    age: 17
+  }, {
+    id: 2,
+    name: 'Luca',
+    age: 22
+  },
+  {
+    id: 3,
+    name: 'Giovanni',
+    age: 19
+  },
+  {
+    id: 4,
+    name: 'Riccardo',
+    age: 28
+  }])
 
   const [toggle,setToggle]=useState(true)
 
@@ -17,6 +38,7 @@ function App() {
     <div className="App">
       {toggle && <Counter onCounterChange={onCounterChange}/>}
      <button onClick={handleButton} >Toggle</button>
+     <FilterdList list={list}/>
     </div>
   );
 }
