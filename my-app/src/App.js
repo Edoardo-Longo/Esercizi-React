5915e5c87147403aa4c089b16a372f05d0b9387d
Edoardo Longo <Edoardo.Longo.1999@gmail.com>
2022-06-30 17:12:58 +0200

    Eserczio 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Welcome from './Welcome';
+import Counter from './Counter';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path='/' element={<Welcome name='Edoardo'/>}></Route>
+        <Route path='/counter' element={<Counter/>}></Route>
       </Routes>
     </div>
   );
